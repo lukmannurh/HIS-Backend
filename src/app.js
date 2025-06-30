e55f763b487008bb07709016361bafabd6862d60
Lukman Nur Hakim <luqmannur33@gmail.com>
2025-06-30 08:51:09 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -15,35 +15,34 @@ const app = express();
 setupSwagger(app);
 
 // app.use(helmet());
-// app.use(cors({
-//   origin: ["http://203.194.112.226"],
-//   methods: ["GET","POST","PUT","DELETE"],
-//   allowedHeaders: ["Content-Type","Authorization"],
-// }));
-
-app.use(cors({
-  origin: 'https://his-frontend-8u1n.vercel.app',
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization']
-}));
-
-// app.use(cors({
-//   origin: '*',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(
+  cors({
+    // origin: '*',
+    origin: [
+      "https://his-frontend-8u1n.vercel.app",
+      "https://www.his-lukman.cloud",
+    ],
+    // origin: 'https://his-frontend-8u1n.vercel.app',
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
 // Sajikan folder uploads sebagai static
-app.use("/uploads", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // atau set sesuai asal yang diizinkan, misalnya "http://localhost:3001"
-  next();
-}, express.static("uploads"));
+app.use(
+  "/uploads",
+  (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // atau set sesuai asal yang diizinkan, misalnya "http://localhost:3001"
+    next();
+  },
+  express.static("uploads")
+);
 
 app.use(
   morgan("combined", {
-    stream: { write: (message) => logger.info(message.trim()) }
+    stream: { write: (message) => logger.info(message.trim()) },
   })
 );
 
